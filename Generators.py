7998f75e4b1ae18c768dd657b13bdaff9f42dbf1
TashikMoin23 <47076116+TashikMoin23@users.noreply.github.com>
2019-07-14 03:14:24 -0400

    Add files via upload

---
+++ Generators.py
@@ -0,0 +1,43 @@
+import memory_profiler as mem_profile
+import random
+import time
+
+names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
+majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
+
+Before=str(mem_profile.memory_usage())
+
+def people_list(num_people):
+    result = []
+    for i in range(num_people):
+        person = {
+                    'id': i,
+                    'name': random.choice(names),
+                    'major': random.choice(majors)
+                }
+        result.append(person)
+    return result
+
+def people_generator(num_people):
+    for i in range(0,num_people):
+        person = {
+                    'id': i,
+                    'name': random.choice(names),
+                    'major': random.choice(majors)
+                }
+        yield person
+
+# t1 = time.clock()
+# people = people_list(10000)
+# t2 = time.clock()
+
+# t1 = time.clock()
+people = people_generator(1000000)
+# t2 = time.clock()
+
+for i in people:
+    print(i)
+
+print('Memory (Before) : ' + Before + 'MB')
+print('Memory (After) : ' + str(mem_profile.memory_usage()) + 'MB')
+# print ('Took ' + str(t2-t1) + ' Seconds')
